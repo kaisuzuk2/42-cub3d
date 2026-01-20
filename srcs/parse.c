af29f99634557b90b4bc3cb7642b2502161a9aac
kaisuzuk <szkakg@gmail.com>
2026-01-20 02:51:20 +0000

    mapread

--- srcs/parse.c
+++ srcs/parse.c
@@ -276,6 +276,83 @@ t_bool detect_player(t_config *conf)
     return (TRUE);
 }
 
+char **dup_map(char **map)
+{
+    int row;
+    char **res;
+    int y;
+
+    row = 0;
+    while (map[row])
+        row++;
+    res = (char **)malloc(sizeof(char *) * (row + 1));
+    if (!res)
+        return (NULL);
+    y = 0;
+    while (y < row)
+    {
+        res[y] = ft_strdup(map[y]);
+        if (!res[y])
+        {
+            while (y > 0)
+                free(res[--y]);
+            free(res);
+            return (NULL);       
+        }
+        y++;
+    }
+    res[y] = NULL;
+    return (res);
+}
+
+t_bool fload_fill(char **map, int x, int y)
+{
+    if (y < 0 || x < 0)
+        return (TRUE);
+    if (!map[y])
+        return (TRUE);
+    if (x >= (int)ft_strlen(map[y]))
+        return (TRUE);
+    
+    if (map[y][x] == ' ')
+        return (TRUE);
+    
+    if (map[y][x] == '1' || map[y][x] == 'V')
+        return (FALSE);
+    
+    map[y][x] = 'V';
+
+    if (fload_fill(map, x, y - 1))
+        return (TRUE);
+    if (fload_fill(map, x, y + 1))
+        return (TRUE);
+    if (fload_fill(map, x + 1, y))
+        return (TRUE);
+    if (fload_fill(map, x - 1, y))
+        return (TRUE);
+    return (FALSE);
+}
+
+t_bool is_map_closed(t_config *conf)
+{
+    char **cpy;
+    t_bool is_leak;
+    int i;
+
+    cpy = dup_map(conf->map);
+    if (!cpy)
+        return (FALSE); // ### TODO: エラー処理
+    is_leak = fload_fill(cpy, conf->player_x, conf->player_y);
+    i = 0;
+    while (cpy[i])
+    {
+        free(cpy[i]);
+        i++;
+    } 
+    free(cpy);
+    return (!is_leak);
+}  
+
 t_bool read_file(char *file_name, t_config *conf)
 {
     const int fd = open(file_name, O_RDONLY);
@@ -288,6 +365,10 @@ t_bool read_file(char *file_name, t_config *conf)
     if (!read_config(fd, conf))
         return (close(fd), FALSE);
     if (!read_map(fd, conf))
+        return (close(fd), FALSE);
+    if (!detect_player(conf))
+        return (close(fd), FALSE);
+    if (!is_map_closed(conf))
         return (close(fd), FALSE);
     close(fd);
     return (TRUE);
