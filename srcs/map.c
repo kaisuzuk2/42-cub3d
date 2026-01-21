12c955a7ee7c13fde952e90fa22d52503fa26317
kaisuzuk <szkakg@gmail.com>
2026-01-21 07:09:27 +0000

    不要なコード削除

--- srcs/map.c
+++ srcs/map.c
@@ -29,46 +29,6 @@
 //     return (map);
 // }
 
-
-char **get_map(void)
-{
-    char **map = malloc(sizeof(char *) * 11);
-    map[0] = "          111111111111111";
-    map[1] = "          100000000000001";
-    map[2] =           "100000000000001";
-    map[3] = "          100011111111111";
-    map[4] = "100000000000001";
-    map[5] = "100000010000001";
-    map[6] = "100001000000001";
-    map[7] = "100000000000001";
-    map[8] = "100000000000001";
-    map[9] = "111111111111111";
-    map[10] = NULL;
-    return (map);
-}
-
-// t_bool map_has_wall_at(double x, double y, char **map) 
-// {
-//     int row;
-//     int col;
-
-//     // if (x < 0 || x >= WIDTH || y < 0 || y >= HEIGHT)
-//     //     return (TRUE);
-//     if (x < 0 || y < 0)
-//         return (TRUE);
-//     row = 0;
-//     while (map[row])
-//         row++;
-//     if ((int)(y / TILE_SIZE) >= row)
-//         return (TRUE);
-//     col = 0;
-//     while (map[(int)(y / TILE_SIZE)][col])
-//         col++;
-//     if ((int)(x / TILE_SIZE) >= col)
-//         return (TRUE);
-//     return (map[(int)(y / TILE_SIZE)][(int)(x / TILE_SIZE)] != '0');
-// }
-
 t_bool map_has_wall_at(double x, double y, char **map)
 {
     int map_x;
@@ -77,33 +37,20 @@ t_bool map_has_wall_at(double x, double y, char **map)
 
     if (!map)
         return (TRUE);
-
-    // ワールド座標が負なら即「外」
     if (x < 0 || y < 0)
         return (TRUE);
-
     map_x = (int)(x / TILE_SIZE);
     map_y = (int)(y / TILE_SIZE);
-
-    // タイル座標が負なら即「外」
     if (map_x < 0 || map_y < 0)
         return (TRUE);
-
-    // 高さを数えて、map_y が範囲外なら「外」
     h = 0;
     while (map[h])
         h++;
     if (map_y >= h)
         return (TRUE);
-
-    // 幅（行長）を見て、map_x が範囲外なら「外」
-    // ※行長がバラバラでも安全
     if (map_x >= (int)ft_strlen(map[map_y]))
         return (TRUE);
-
-    // 空白は「外(=壁扱い)」に統一するのが安定
     if (map[map_y][map_x] == ' ')
         return (TRUE);
-
     return (map[map_y][map_x] == '1');
 }
--- srcs/render.c
+++ srcs/render.c
@@ -30,65 +30,6 @@ static void draw_background_strip(t_game *game, int x, int top, int bottom)
     }
 }
 
-// static void render_3d_walls(t_game *game)
-// {
-//     const float fov = PI / 3.0f;
-//     const float dist_proj_plane = (WIDTH / 2.0f) / tanf(fov / 2.0f);
-
-//     for (int i = 0; i < NUM_RAYS; i++)
-//     {
-//         // fisheye
-//         // cos = 正面 / dist
-//         float perp_dist = game->player.ray[i].distance * cosf(game->player.ray[i].ray_angle - game->player.angle);
-
-//         // 壁が近すぎる時の補正
-//         if (perp_dist < 0.0001f)
-//             perp_dist = 0.0001f;
-
-//         // 壁の高さ
-//         float proj_wall_h = (TILE_SIZE / perp_dist) * dist_proj_plane;
-//         int wall_h = (int)proj_wall_h;
-
-//         int top = (HEIGHT / 2) - (wall_h / 2);
-//         int bottom = (HEIGHT / 2) + (wall_h / 2);
-
-//         // テクスチャ
-//         float hit = game->player.ray[i].was_hit_vertical ? game->player.ray[i].wall_hit_y: 
-//                                                                 game->player.ray[i].wall_hit_x;
-//         t_tex *tex = select_wall_tex(game, &game->player.ray[i]);
-//         int tex_x = (int)fmodf(hit, TILE_SIZE);
-//         tex_x = (int)((float)tex_x * tex->w / TILE_SIZE);
-
-//         if (top < 0)
-//             top = 0;
-//         if (bottom >= HEIGHT)
-//             bottom = HEIGHT - 1;
-//         // 天井
-//         for (int y = 0; y < top; y++)
-//             put_pixel(&game->img, i, y, game->conf.ceil_color);
-        
-//         // 壁
-//         for (int y = top; y <= bottom; y++)
-//         {
-//             int dist_from_top = y - top;
-//             // テクスチャの座標へスケール
-//             int tex_y = (int)((float)dist_from_top * tex->h / (bottom - top + 1));
-
-//             unsigned int color = get_texel(tex, tex_x, tex_y);
-//             put_pixel(&game->img, i, y, color);
-//         }
-
-//         // 床
-//         for (int y = bottom + 1; y < HEIGHT; y++)
-//             put_pixel(&game->img, i, y, game->conf.floor_color);
-//         // int color = game->player.ray[i].was_hit_vertical ? 0x00CCCC: 0x00FFFF;
-
-
-//         // draw_wall_split(&game->img, i, top, bottom, color);
-//     }
-// }
-
-
 // fisheye
 // cos = fromt / dist
 static float calc_perp_dist(t_ray *ray, float player_angle)
@@ -128,7 +69,6 @@ static int get_tex_x(t_ray *ray, t_tex *tex)
     else
         hit = ray->wall_hit_x;
     tex_x = (int)fmodf(hit, TILE_SIZE);
-    // これ微妙
     tex_x = (int)((float)tex_x * tex->w / TILE_SIZE);
     return (tex_x);
 }
@@ -147,7 +87,7 @@ static void draw_wall_split(t_game *game, int i, int top, int bottom)
     while (y <= bottom)
     {
         dist_from_top = y - top;
-        tex_y = (int)((float)dist_from_top * tex->h / (bottom - top + 1)); // 微妙
+        tex_y = (int)((float)dist_from_top * tex->h / (bottom - top + 1)); 
         put_pixel(&game->img, i, y, get_texel(tex, tex_x, tex_y));
         y++;
     }
