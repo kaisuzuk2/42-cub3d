8e1b6865b742742a8711a3a2dfbd979429464584
kaisuzuk <szkakg@gmail.com>
2026-01-21 02:06:47 +0000

    player_init

--- main.c
+++ main.c
@@ -18,10 +18,10 @@ int main(void)
     t_game game;
     if (!read_file("test.cub", &game.conf))
         return (1); // ### TODO: エラー処理
-    printf("%s\n", game.conf.path_n);
     if (!init(&game))
         return (1); // ### TODO: エラー処理
     init_player(&game.player, &game.conf);
+    printf("%d %d\n", game.conf.player_x, game.conf.player_y);
     mlx_hook(game.win, KEYPRESS, 1L<<0, key_press, &game.player);
     mlx_hook(game.win, KEYRELEASE, 1L<<1, key_release, &game.player);
     mlx_loop_hook(game.mlx, render, &game);
