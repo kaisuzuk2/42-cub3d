69ebacf09de3013c2b5463d5ff6f13a442dcccf8
Kaisei Suzuki <kaisuzuk@c3r6s2.42tokyo.jp>
2026-01-28 16:11:23 +0900

    エラー出力をpfdに合わせて修正

--- srcs/utils/error.c
+++ srcs/utils/error.c
@@ -6,7 +6,7 @@
 /*   By: kaisuzuk <kaisuzuk@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/23 14:24:20 by kaisuzuk          #+#    #+#             */
-/*   Updated: 2026/01/27 15:34:45 by kaisuzuk         ###   ########.fr       */
+/*   Updated: 2026/01/28 16:10:25 by kaisuzuk         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,27 +14,27 @@
 
 void	print_usage(char *str)
 {
-	ft_putstr_fd(ERR_LABEL, STDERR_FILENO);
+	ft_putendl_fd(ERR_LABEL, STDERR_FILENO);
 	ft_putendl_fd(str, STDERR_FILENO);
 	ft_putendl_fd("Usage: ./cub3D filename.cub", STDERR_FILENO);
 }
 
 void	print_error(char *label, char *str)
 {
-	ft_putstr_fd(ERR_LABEL, STDERR_FILENO);
+	ft_putendl_fd(ERR_LABEL, STDERR_FILENO);
 	ft_putstr_fd(label, STDERR_FILENO);
 	ft_putendl_fd(str, STDERR_FILENO);
 }
 
 void	print_sys_error(char *func)
 {
-	ft_putstr_fd(ERR_LABEL, STDERR_FILENO);
+	ft_putendl_fd(ERR_LABEL, STDERR_FILENO);
 	perror(func);
 }
 
 void	print_error_detail(char *label, char *str, char *near)
 {
-	ft_putstr_fd(ERR_LABEL, STDERR_FILENO);
+	ft_putendl_fd(ERR_LABEL, STDERR_FILENO);
 	ft_putstr_fd(label, STDERR_FILENO);
 	ft_putstr_fd(str, STDERR_FILENO);
 	ft_putstr_fd(" near ", STDERR_FILENO);
